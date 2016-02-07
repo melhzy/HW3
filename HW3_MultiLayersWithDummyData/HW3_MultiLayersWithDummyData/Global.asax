e7f945fcc97ba618db113376b1adc0a2379626af
melhzy <zhuang@sqlgeek.org>
2016-02-07 00:13:40 -0500

    additional commit

---
+++ HW3_MultiLayersWithDummyData/HW3_MultiLayersWithDummyData/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HW3_MultiLayersWithDummyData.MvcApplication" Language="C#" %>
---
+++ HW3_MultiLayersWithDummyData/HW3_MultiLayersWithDummyData/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HW3_MultiLayersWithDummyData.Startup))]
+namespace HW3_MultiLayersWithDummyData
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
